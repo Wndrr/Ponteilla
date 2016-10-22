c884029b8bb26782a95b6c6f0438f0ba0b57ce46
Wndrr <mathieu.viales@gmail.com>
2016-10-22 17:08:56 +0200

    v0 - Reformat email
    - Send HTML email 
    - Refactor email sending fuction
    - Comment email sending fuction

--- src/controllers/ajax.controller.php
+++ src/controllers/ajax.controller.php
@@ -3,7 +3,7 @@
  * @Author: Wndrr
  * @Date:   2016-09-21 20:25:50
  * @Last Modified by:   Wndrr
- * @Last Modified time: 2016-10-11 22:19:25
+ * @Last Modified time: 2016-10-22 17:05:37
  */
 
 use Symfony\Component\HttpFoundation\Request;
@@ -17,25 +17,64 @@ $ajax = $app['controllers_factory'];
 	/*----------  Send contact e-mail  ----------*/
 	$ajax->post('/ajaxValidation/contact', function(Request $r) use($app)
 	{
-		$mail = $app['Mailer']();
+		/*==================================
+		=            Build vars            =
+		==================================*/
 		
-	    $mail->Subject = "Demande d'informations";
-	    $sections = implode(', ', $r->get('section'));
-		$mail->Body    = "Demande d'informations de la part de  {$r->get('firstName')} {$r->get('lastName')}, adresse email : {$r->get('email')}, \n\rLa demande concerne la ou les sections suivantes ; {$sections} \n\r{$r->get('message')}";
-	    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+			$firstName = ucfirst($r->get('firstName'));
+			$lastName = strtoupper($r->get('lastName'));
+			$email = $r->get('email');
+		    $sections = implode(', ', $r->get('section'));
+			$message = $r->get('message');
+			$date = date('d/m/Y - H:i');
+		
+		/*=====  End of Create vars  =====*/
+		
+		/*===========================================
+		=            Build email content            =
+		===========================================*/
+		
+			$mailContent = 
+			"<div style='text-align:center'>
+				<h1>
+					Demande d'informations
+				</h1>
+				<h2>
+					Mail automatic - formularie de contact
+				</h2>
+				Demande de la part de $lastName $firstName, le $date.
+				<br />
+				Répondre à : $email
+				<br />
+				Sections concernées : $sections.
+				<div style='width: 70%; margin-left:15%'>
+					<hr />
+					<h3>
+						Message
+					</h3>
+					$message
+				</div>
+			</div>";
+		
+		/*=====  End of Build email content  ======*/
+		
+		$mail = $app['Mailer']();
 
+		$mail->isHtml(true);	
+	    $mail->CharSet = 'UTF-8';
+
+	    $mail->AddReplyTo($email, "$lastName $firstName");
+
+	    $mail->Subject = "Formularie de contact";		    
+		$mail->Body = $mailContent;
 
 		if($app['debug'])
 			$mail->addAddress("mathieu.viales@gmail.com");
-
 		else
 			$mail->addAddress("sectionrando.frponteillanyls@gmail.com");
 
-
-
 	    if($mail->send()) 
-			return $app['twig']->render('/sections/_shared/contactSuccess.partial.html.twig');
-	    
+			return $app['twig']->render('/sections/_shared/contactSuccess.partial.html.twig');	    
 	    else 
 	        return 'Message could not be sent.' + 'Mailer Error: ' . $mail->ErrorInfo;
 	    
